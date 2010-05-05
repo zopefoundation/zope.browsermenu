392082dde370ecd849eca50eafbb2e04de903d8b
Tres Seaver <tseaver@palladion.com>
2010-05-05 17:32:08 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2007 Zope Corporation and Contributors.
+# Copyright (c) 2007 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -21,7 +21,7 @@ long_description = (open('README.txt').read() + '\n\n' +
 setup(name='zope.browsermenu',
       version = '3.9.2dev',
       url='http://pypi.python.org/pypi/zope.browsermenu/',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       classifiers = ['Environment :: Web Environment',
                      'Intended Audience :: Developers',
@@ -34,6 +34,7 @@ setup(name='zope.browsermenu',
       description='Browser menu implementation for Zope.',
       long_description=long_description,
 
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir={'': 'src'},
 
