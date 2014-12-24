fe13d4e3533bbe26d7c0024682486293381fa6d9
Tres Seaver <tseaver@palladion.com>
2014-12-24 10:32:39 -0500

    Target 4.1.0 as upcoming release.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.browsermenu',
-      version='4.1.0a2.dev0',
+      version='4.1.0.dev0',
       url='http://pypi.python.org/pypi/zope.browsermenu/',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
