0f163e36c77e6b37624ee8f8ce82b17d6a61e546
Jason Madden <jamadden@gmail.com>
2015-06-05 12:08:44 -0500

    Add support for Python3.2

--- src/zope/browsermenu/metadirectives.py
+++ src/zope/browsermenu/metadirectives.py
@@ -21,40 +21,41 @@ from zope.security.zcml import Permission
 
 from zope.component.zcml import IBasicViewInformation
 from zope.browsermenu.field import MenuField
+from ._compat import _u
 
 
 class IMenuDirective(Interface):
     """Define a browser menu"""
 
     id = TextLine(
-        title=u"The name of the menu.",
-        description=u"This is, effectively, an id.",
+        title=_u("The name of the menu."),
+        description=_u("This is, effectively, an id."),
         required=False
         )
 
     title = MessageID(
-        title=u"Title",
-        description=u"A descriptive title for documentation purposes",
+        title=_u("Title"),
+        description=_u("A descriptive title for documentation purposes"),
         required=False
         )
-    
+
     description = MessageID(
-        title=u"Description",
-        description=u"A description title of the menu.",
+        title=_u("Description"),
+        description=_u("A description title of the menu."),
         required=False
         )
 
     class_ = GlobalObject(
-        title=u"Menu Class",
-        description=u"The menu class used to generate the menu.",
+        title=_u("Menu Class"),
+        description=_u("The menu class used to generate the menu."),
         required=False
         )
 
     interface = GlobalInterface(
-        title=u"The menu's interface.",
+        title=_u("The menu's interface."),
         required=False
         )
-    
+
 
 class IMenuItemsDirective(Interface):
     """
@@ -65,31 +66,31 @@ class IMenuItemsDirective(Interface):
     """
 
     menu = MenuField(
-        title=u"Menu name",
-        description=u"The (name of the) menu the items are defined for",
+        title=_u("Menu name"),
+        description=_u("The (name of the) menu the items are defined for"),
         required=True,
         )
 
     for_ = GlobalObject(
-        title=u"Interface",
-        description=u"The interface the menu items are defined for",
+        title=_u("Interface"),
+        description=_u("The interface the menu items are defined for"),
         required=True
         )
 
     layer = GlobalInterface(
-        title=u"Layer",
-        description=u"The Layer for which the item is declared.",
+        title=_u("Layer"),
+        description=_u("The Layer for which the item is declared."),
         required=False
         )
 
     permission = Permission(
-        title=u"The permission needed access the item",
-        description=u"""
+        title=_u("The permission needed access the item"),
+        description=_u("""
         This can usually be inferred by the system, however, doing so
         may be expensive. When displaying a menu, the system tries to
         traverse to the URLs given in each action to determine whether
         the url is accessible to the current user. This can be
-        avoided if the permission is given explicitly.""",
+        avoided if the permission is given explicitly."""),
         required=False
         )
 
@@ -99,39 +100,39 @@ class IMenuItem(Interface):
     """
 
     title = MessageID(
-        title=u"Title",
-        description=u"The text to be displayed for the menu item",
+        title=_u("Title"),
+        description=_u("The text to be displayed for the menu item"),
         required=True
         )
 
     description = MessageID(
-        title=u"A longer explanation of the menu item",
-        description=u"""
+        title=_u("A longer explanation of the menu item"),
+        description=_u("""
         A UI may display this with the item or display it when the
-        user requests more assistance.""",
+        user requests more assistance."""),
         required=False
         )
 
     icon = TextLine(
-        title=u"Icon Path",
-        description=u"Path to the icon resource representing this menu item.",
+        title=_u("Icon Path"),
+        description=_u("Path to the icon resource representing this menu item."),
         required=False
         )
 
     permission = Permission(
-        title=u"The permission needed access the item",
-        description=u"""
+        title=_u("The permission needed access the item"),
+        description=_u("""
         This can usually be inferred by the system, however, doing so
         may be expensive. When displaying a menu, the system tries to
         traverse to the URLs given in each action to determine whether
         the url is accessible to the current user. This can be
-        avoided if the permission is given explicitly.""",
+        avoided if the permission is given explicitly."""),
         required=False
         )
 
     filter = TextLine(
-        title=u"A condition for displaying the menu item",
-        description=u"""
+        title=_u("A condition for displaying the menu item"),
+        description=_u("""
         The condition is given as a TALES expression. The expression
         has access to the variables:
 
@@ -142,21 +143,21 @@ class IMenuItem(Interface):
         nothing -- None
 
         The menu item will not be displayed if there is a filter and
-        the filter evaluates to a false value.""",
+        the filter evaluates to a false value."""),
         required=False
         )
 
     order = Int(
-        title=u"Order",
-        description=u"A relative position of the menu item in the menu.",
+        title=_u("Order"),
+        description=_u("A relative position of the menu item in the menu."),
         required=False,
         default=0
         )
 
     item_class = GlobalObject(
-        title=u"Menu item class",
-        description=u"""
-        A class to be used as a factory for creating menu item""",
+        title=_u("Menu item class"),
+        description=_u("""
+        A class to be used as a factory for creating menu item"""),
         required=False
         )
 
@@ -164,10 +165,10 @@ class IMenuItemSubdirective(IMenuItem):
     """Define a menu item within a group of menu items"""
 
     action = TextLine(
-        title=u"The relative url to use if the item is selected",
-        description=u"""
+        title=_u("The relative url to use if the item is selected"),
+        description=_u("""
         The url is relative to the object the menu is being displayed
-        for.""",
+        for."""),
         required=True
         )
 
@@ -178,23 +179,23 @@ class ISubMenuItemSubdirective(IMenuItem):
     """Define a menu item that represents a a sub menu.
 
     For a sub-menu menu item, the action is optional, this the item itself
-    might not represent a destination, but just an entry point to the sub menu. 
+    might not represent a destination, but just an entry point to the sub menu.
     """
 
     action = TextLine(
-        title=u"The relative url to use if the item is selected",
-        description=u"""
+        title=_u("The relative url to use if the item is selected"),
+        description=_u("""
         The url is relative to the object the menu is being displayed
-        for.""",
+        for."""),
         required=False
         )
 
     submenu = TextLine(
-        title=u"Sub-Menu Id",
-        description=u"The menu that will be used to provide the sub-entries.",
+        title=_u("Sub-Menu Id"),
+        description=_u("The menu that will be used to provide the sub-entries."),
         required=True,
         )
-    
+
 class ISubMenuItemDirective(IMenuItemsDirective, ISubMenuItemSubdirective):
     """Define one menu item"""
 
@@ -202,39 +203,39 @@ class IAddMenuItemDirective(IMenuItem):
     """Define an add-menu item"""
 
     for_ = GlobalInterface(
-        title=u"Interface",
-        description=u"The interface the menu items are defined for",
+        title=_u("Interface"),
+        description=_u("The interface the menu items are defined for"),
         required=False
         )
 
     class_ = GlobalObject(
-        title=u"Class",
-        description=u"""
-        A class to be used as a factory for creating new objects""",
+        title=_u("Class"),
+        description=_u("""
+        A class to be used as a factory for creating new objects"""),
         required=False
         )
 
     factory = Id(
-        title=u"Factory",
-        description=u"A factory id for creating new objects",
+        title=_u("Factory"),
+        description=_u("A factory id for creating new objects"),
         required = False,
         )
 
     view = TextLine(
-        title=u"Custom view name",
-        description=u"The name of a custom add view",
+        title=_u("Custom view name"),
+        description=_u("The name of a custom add view"),
         required = False,
         )
 
     menu = MenuField(
-        title=u"Menu name",
-        description=u"The (name of the) menu the items are defined for",
+        title=_u("Menu name"),
+        description=_u("The (name of the) menu the items are defined for"),
         required=False,
         )
 
     layer = GlobalInterface(
-        title=u"The layer the custom view is declared for",
-        description=u"The default layer for which the custom view is "
-                    u"applicable. By default it is applied to all layers.",
+        title=_u("The layer the custom view is declared for"),
+        description=_u("The default layer for which the custom view is "
+                       "applicable. By default it is applied to all layers."),
         required=False
         )
