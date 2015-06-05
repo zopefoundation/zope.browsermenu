6c29cdb8b1c498969c766efee373057e2849c0b9
Tres Seaver <tseaver@palladion.com>
2015-06-05 16:49:21 -0400

    svb

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.browsermenu',
-      version='4.1.1',
+      version='4.1.2.dev0',
       url='http://pypi.python.org/pypi/zope.browsermenu/',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
