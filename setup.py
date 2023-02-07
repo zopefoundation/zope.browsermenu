41cffdb18892ba892bcd6cafe4be8c95dd5bc3ac
Michael Howitz <mh@gocept.com>
2023-02-07 12:24:30 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.browsermenu',
-    version='4.5.dev0',
+    version='5.0.dev0',
     url='http://github.com/zopefoundation/zope.browsermenu/',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
