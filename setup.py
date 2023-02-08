60d06263be21d4ac77323b6f8a8c282bb1c9797c
Michael Howitz <mh@gocept.com>
2023-02-08 16:08:05 +0100

    Preparing release 5.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.browsermenu',
-    version='5.0.dev0',
+    version='5.0',
     url='http://github.com/zopefoundation/zope.browsermenu/',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
