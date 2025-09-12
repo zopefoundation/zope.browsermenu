68735ca98746eed765ea358ac9d1fd788305177e
Michael Howitz <icemac@gmx.net>
2025-09-12 08:29:37 +0200

    Switch to PEP 420 native namespace. (#20)
    * Configuring for pure-python
    
    * Bumped version for breaking release.
    
    * Replace ``pkg_resources`` namespace with PEP 420 native namespace.
    
    * Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@
 """
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -27,12 +26,12 @@ def read(*rnames):
 long_description = (read('README.rst') + '\n\n' + read('CHANGES.rst'))
 TESTS_REQUIRE = [
     'zope.testing',
-    'zope.testrunner',
+    'zope.testrunner >= 6.4',
 ]
 
 setup(
     name='zope.browsermenu',
-    version='5.2.dev0',
+    version='6.0.dev0',
     url='http://github.com/zopefoundation/zope.browsermenu/',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
@@ -56,9 +55,6 @@ setup(
     description='Browser menu implementation for Zope.',
     long_description=long_description,
     license='ZPL-2.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    namespace_packages=['zope'],
     python_requires='>=3.9',
     install_requires=[
         'setuptools',
